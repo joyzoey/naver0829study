26ad32992752bb08ce265a5435ae9562a7f11cea
kimnahye <skgp9150@gmail.com>
2023-11-14 10:07:53 +0900

    1114

---
+++ springwork/SpringReadMVCEx4/src/main/java/study/spring/day1113/ShopDto.java
@@ -0,0 +1,14 @@
+package study.spring.day1113;
+
+import lombok.Data;
+
+@Data
+public class ShopDto {
+
+	private String sang;
+	private String photo;
+	private String color;
+	private int su;
+	private int dan;
+	
+}
